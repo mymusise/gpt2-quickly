0139c2b796e88ab91bc1040ddd6bbb5b8d591e32
mymusise <mymusise1@gmail.com>
2020-11-05 16:43:29 +0800

    update configs
    Signed-off-by: mymusise <mymusise1@gmail.com>

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,8 +1,8 @@
 import os
 
-if os.environ.get('ENV', 'DEV') == 'DEV':
-    from .test import *
-else:
+if os.environ.get('ENV', 'PRO') == 'PRO':
     from .train import *
+else:
+    from .test import *
 
 __all__ = ['path', 'model_path', 'configs', 'data']
