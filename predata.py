770275f8c9efc1ad8a1781464296f3ffb2455633
mymusise <mymusise1@gmail.com>
2020-11-05 21:21:05 +0800

    before add multiply decode
    Signed-off-by: mymusise <mymusise1@gmail.com>

--- predata.py
+++ predata.py
@@ -1,25 +1,50 @@
+from numpy.core.shape_base import block
 import tensorflow as tf
 from transformers import BertTokenizer
 from train import load_tokenizer
 import pickle
 import configs
+import numpy as np
+from tqdm import tqdm
+from multiprocessing import Pool
+
+
+def add_token(texts, ids, tokenizer):
+    return
+
+
+def multiply_decode(handler, tasks, workers=None, call_back=None):
+    if workers == None:
+        workers = len(tasks)
+    p = Pool(workers)
+    for i in range(len(tasks)):
+        p.apply_async(handler, tasks[i])
+    p.close()
+    p.join()
+    print("[all_task done]")
 
 
 def preprocess():
     block_size = configs.model.max_length
     tokenizer = load_tokenizer()
+    input_ids = []
     with open(configs.data.raw, 'r') as f:
-        data = f.read()
-        tokenized_text = tokenizer(data, max_length=len(
-            data), return_tensors='np')['input_ids']
-        data_len = int(tokenized_text.shape[1] // block_size)
-        print(len(data), tokenized_text.shape, data_len, block_size)
-        input_ids = tokenized_text[:, :block_size * data_len]
-        input_ids = input_ids.reshape((data_len, block_size))
+        data = f.read().replace('  ', ' ').replace('\n\n', '\n')
+        print(0, len(data), block_size)
+        texts = []
+        for i in tqdm(range(0, len(data), block_size)):
+            text = data[i:i+block_size]
+            text_encoded = tokenizer(text)['input_ids']
+            input_ids += text_encoded
+    print(len(data), len(input_ids))
+    text_len = len(input_ids) // block_size
+    input_ids = np.array(input_ids)
+    input_ids.resize(text_len * block_size)
+    input_ids = input_ids.reshape((text_len, block_size))
 
     ids = input_ids[:, :-1]
     labels = input_ids[:, 1:]
-
+    print(f"ids.shape: {ids.shape}  labels.shape: {labels.shape}")
     pickle.dump((ids, labels), open(configs.data.pickle, 'wb'))
 
 
