828791046279a83973d489f03cf5149c2182cb38
mymusise <mymusise1@gmail.com>
2021-02-27 18:32:39 +0800

    add protoken for tokenizer builder
    Signed-off-by: mymusise <mymusise1@gmail.com>

--- build_tokenizer.py
+++ build_tokenizer.py
@@ -9,13 +9,13 @@ tokenizer = Tokenizer(BPE())
 trainer = WordPieceTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
 
 def main():
-    tokenizer.train(trainer, [configs.data.raw])
+    tokenizer.train(trainer, [configs.data.raw_cut])
     tokenizer.save(os.path.join(configs.data.path, 'bpe.vocab'))
     print(f"save to {configs.data.path}")
 
 
 def train_with_sentenceprices(vocab_size: int = 3000, num_threads=2, character_coverage=0.98):
-    os.system(f"spm_train --input={configs.data.raw} --model_prefix=spiece --model_type=bpe --character_coverage={character_coverage} --vocab_size={vocab_size} --num_threads={num_threads}")
+    os.system(f"spm_train --input={configs.data.raw_cut} --model_prefix=spiece --model_type=bpe --character_coverage={character_coverage} --vocab_size={vocab_size} --num_threads={num_threads}")
     os.system(f"mv spiece.model {configs.data.path}")
 
 
---
+++ cut_words.py
@@ -0,0 +1,82 @@
+import configs
+import jieba
+from tqdm import tqdm
+import click
+from typing import List
+import os
+from multiprocessing import Process, Manager, Queue
+
+
+def cut_words(processer_num, text, result_dict):
+    with open(os.path.join(configs.data.path, f'raw.cut.temp.{processer_num}.txt'), 'w') as out_f:
+        texts = text.split('\n')
+        for line in tqdm(texts):
+            try:
+                cuts = " ".join(jieba.cut(line))
+                out_f.write(cuts+'\n')
+            except UnicodeDecodeError:
+                pass
+            except KeyError:
+                pass
+            except Exception as e:
+                pass
+
+
+def multiply_cut(handler, tasks):
+    manager = Manager()
+    result_dict = manager.dict()  # didn't work and don't know why
+    jobs = []
+    for processer_num, task in enumerate(tasks):
+        p = Process(target=handler, args=(
+            processer_num, task, result_dict))
+        jobs.append(p)
+
+    for job in jobs:
+        job.start()
+
+    for job in jobs:
+        job.join()
+
+    for job in jobs:
+        try:
+            job.close()  # It may raise exception in python <=3.6
+        except:
+            pass
+    print("[all_task done]")
+
+
+def split_data(
+        text,
+        n_processes
+) -> List[str]:
+    text_task = []
+    num_pre_task = len(text) // n_processes
+    for i in range(0, len(text), num_pre_task):
+        text_task.append(text[i: i + num_pre_task])
+    return text_task
+
+
+@click.command()
+@click.option('--n_processes', default=1, help='Number of processes.')
+def preprocess(n_processes):
+    print(f'reading {configs.data.raw}')
+    with open(configs.data.raw, 'r') as f:
+        data = f.read().replace('  ', ' ').replace('\n\n', '\n')
+        print(f"total words: {len(data)}")
+
+    print(f"split data into {n_processes} pieces")
+    text_task = split_data(data, n_processes)
+
+    multiply_cut(cut_words, text_task)
+
+    with open(configs.data.raw_cut, 'w') as all_cut_file:
+        for i in range(n_processes+1):
+            filename = os.path.join(configs.data.path, f'raw.cut.temp.{i}.txt')
+            with open(filename) as cut_file:
+                all_cut_file.write(cut_file.read()+'\n')
+                print(f'dropping {filename}')
+                os.system(f'rm {filename}')
+
+
+if __name__ == '__main__':
+    preprocess()
