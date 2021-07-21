72685d908aa91096c6bcc3b56e9c5744747bbae8
jshongtw <jshong@ncnu.edu.tw>
2021-07-21 21:30:29 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,13 +6,14 @@ tokenizer = load_tokenizer()
 model = init_model(tokenizer)
 
 text_generator = TextGenerationPipeline(model, tokenizer)
-seed = '國文的世界真美妙'
-
-
-for i in range(20):
-  glist = text_generator(seed, seed, max_length=100, do_sample=True, top_k=30, repetition_penalty=2.0)
-  gtext = glist[0]["generated_text"]
-  if seed in gtext:
-    gtext = gtext.replace(seed,'')
-  print(gtext)
-  seed = gtext[-20:]
+seed = ['這是我第一次嘗試寫','生活中充滿著大大小小的','108上學期小王子電子書','起初我聽朋友介紹','本作品參加108學年度','參加中學生閱讀心得']
+print(seed)
+for j in range(5):
+  input=seed[j]
+  for i in range(5):
+    glist = text_generator(input, max_length=100, do_sample=True, top_k=10, repetition_penalty=2.0)
+    gtext = glist[0]["generated_text"]
+    if input in gtext:
+      gtext = gtext.replace(input,'')
+    print(gtext)
+    input = gtext[-20:]
