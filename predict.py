278924e65a442a8b81517114af5db4c4ee421e0c
jshongtw <jshong@ncnu.edu.tw>
2021-07-21 22:26:07 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,11 +6,11 @@ tokenizer = load_tokenizer()
 model = init_model(tokenizer)
 
 text_generator = TextGenerationPipeline(model, tokenizer)
-seed = ['這是我第一次嘗試寫','生活中充滿著大大小小的','108上學期小王子電子書','起初我聽朋友介紹','本作品參加108學年度','參加中學生閱讀心得']
+seed = ['這是我第一次嘗試寫魯迅','我真心參加了閱讀心得比賽','我的離島記憶。','讀完一本書後，把自己的感想跟心得打出來','獨享，獨自享受，不願和其他人分享。']
 print(seed)
-for j in range(5):
+for j in range(3):
   input=seed[j]
-  for i in range(5):
+  for i in range(1):
     glist = text_generator(input, max_length=100, do_sample=True, top_k=10, repetition_penalty=2.0)
     gtext = glist[0]["generated_text"]
     if input in gtext:
