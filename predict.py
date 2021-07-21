0ad208ae0ccd3e0d17dc16879fd931a10a53be30
jshongtw <jshong@ncnu.edu.tw>
2021-07-21 20:30:56 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,8 +6,13 @@ tokenizer = load_tokenizer()
 model = init_model(tokenizer)
 
 text_generator = TextGenerationPipeline(model, tokenizer)
-print(text_generator("國文的世界真美妙", max_length=100, do_sample=True, top_k=10, eos_token_id=tokenizer.get_vocab().get("】", 0)))
-print(text_generator("國文的世界真美妙", max_length=100, do_sample=True, top_k=20, eos_token_id=tokenizer.get_vocab().get("】", 0)))
-print(text_generator("國文的世界真美妙", max_length=100, do_sample=True, top_k=30, eos_token_id=tokenizer.get_vocab().get("】", 0)))
-print(text_generator("國文的世界真美妙", max_length=100, do_sample=False))
-print(text_generator("國文的世界真美妙", max_length=100, do_sample=False))
+seed = '國文的世界真美妙'
+
+
+for i in range(20):
+  glist = text_generator(seed, seed, max_length=100, do_sample=True, top_k=30, repetition_penalty=2.0)
+  gtext = glist[0]["generated_text"]
+  if seed in gtext:
+    gtext = gtext.replace(seed,'')
+  print(gtext)
+  seed = gtext[-20:]
